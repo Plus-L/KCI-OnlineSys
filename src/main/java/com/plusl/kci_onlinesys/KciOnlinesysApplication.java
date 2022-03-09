e57e7b6cd3e5acb36279afcd6ab9aa49d60671dc
JiaHao LI <2732457087@qq.com>
2022-03-09 10:17:53 +0800

    init repo

---
+++ src/main/java/com/plusl/kci_onlinesys/KciOnlinesysApplication.java
@@ -0,0 +1,13 @@
+package com.plusl.kci_onlinesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KciOnlinesysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KciOnlinesysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plusl/kci_onlinesys/KciOnlinesysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plusl.kci_onlinesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KciOnlinesysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
